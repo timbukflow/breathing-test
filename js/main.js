9eb3315b176dd75bb8350669a6d327d724056149
timbuk <ivoschwizer@gmail.com>
2019-03-19 18:21:02 +0100

    nav

--- js/main.js
+++ js/main.js
@@ -1,28 +1,10 @@
 $(document).ready(function () {
     
-    //Responsive Navigation
+    //nav
     
-    $('.menu').click(function() {
-        $('.menu').hide();
-        $('.topnav .navresp').css('display', 'block');
-        $('.close').css('display', 'block');
-    });
-    
-     $('.close').click(function() {
-        $('.menu').show();
-        $('.topnav .navresp').hide();
-        $('.close').hide();
-    });
-    
-    
-    //Sroll resizing Navigation
-    
-    $(document).on('scroll', function () {
-        if ($(document).scrollTop() > 10) {
-        $('#navcontainer').css('padding', '20px 5%'); 
-    }   else {
-        $('#navcontainer').css('padding', '40px 5%'); 
-    }
-    });
+	$('#navburger').click(function(){
+		$(this).toggleClass('open');
+        $('.nav').toggleClass('on');
+	});
     
 });
