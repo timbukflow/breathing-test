deeb0f1c5460a3e962a33d3c044697cfa7450968
Ivo-Schwizer <ivo@schwizerschlatter.com>
2019-03-19 16:37:18 +0100

    startseite

--- js/main.js
+++ js/main.js
@@ -1,52 +1,28 @@
 $(document).ready(function () {
-
-    // slider function //
- 
-    $('.buttonNext').click(function() {
-
-        var currentImg1         = $('.active1');
-        var currentafterImg1    = $('.after1');
-        var nextImg1            = currentImg1.next();
-        var afterImg1           = currentImg1.after();
-            
-        var currentText         = $('.activetext')
-        var nextText            = currentText.next();
-        
-        var currentImg2         = $('.active2');
-        var currentafterImg2    = $('.after2');
-        var nextImg2            = currentImg2.next();
-        var afterImg2           = currentImg2.after();
-        
-        
-        if (nextImg1.length) {
-            nextImg1.addClass('active1');
-            afterImg1.addClass('after1');
-            
-            nextText.addClass('activetext')
-            
-            nextImg2.addClass('active2');
-            afterImg2.addClass('after2');
-        }
-            
-        else {
-            $('.img-first1').first().addClass('active1');
-            $('.img-first1').last().addClass('after1');
-            
-            $('.text').first().addClass('activetext');
-            
-            $('.img-first2').first().addClass('active2');
-            $('.img-first2').last().addClass('after2');
-        }
-        
-        currentImg1.removeClass('active1');
-        currentafterImg1.removeClass('after1');
-            
-        currentText.removeClass('activetext');
-        
-        currentImg2.removeClass('active2');
-        currentafterImg2.removeClass('after2');
+    
+    //Responsive Navigation
+    
+    $('.menu').click(function() {
+        $('.menu').hide();
+        $('.topnav .navresp').css('display', 'block');
+        $('.close').css('display', 'block');
     });
-     
+    
+     $('.close').click(function() {
+        $('.menu').show();
+        $('.topnav .navresp').hide();
+        $('.close').hide();
+    });
+    
+    
+    //Sroll resizing Navigation
+    
+    $(document).on('scroll', function () {
+        if ($(document).scrollTop() > 10) {
+        $('#navcontainer').css('padding', '20px 5%'); 
+    }   else {
+        $('#navcontainer').css('padding', '40px 5%'); 
+    }
+    });
+    
 });
-
-
