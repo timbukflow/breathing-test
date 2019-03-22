6ae8a26268cd4fab49e5a798529793361e55320e
timbuk <ivoschwizer@gmail.com>
2019-03-22 15:20:04 +0100

    new data

--- js/main.js
+++ js/main.js
@@ -7,7 +7,12 @@ $(document).ready(function () {
         $('.nav').toggleClass('on');
         $('.nav ul').toggleClass('up');
         $('.nav li').toggleClass('open');
-        
 	});
+    
+    $(function () {
+    setInterval(function () {
+        $('.top').fadeOut(1500).delay(5000).fadeIn(1500).delay(5000);
+    }, 5000);
+});
     
 });
